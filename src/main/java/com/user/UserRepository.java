c913e31b728048e1b9e55f479f6a4fdbfba08585
Parker Lillywhite <parkerlillywhite@gmail.com>
2023-10-13 12:34:41 -0600

    login bug fix

--- src/main/java/com/user/UserRepository.java
+++ src/main/java/com/user/UserRepository.java
@@ -7,7 +7,6 @@ import org.springframework.stereotype.Service;
 
 import java.util.Optional;
 
-@Repository
 public interface UserRepository extends JpaRepository<User, Integer> {
     Optional<User> findByEmail(String email);
 }
