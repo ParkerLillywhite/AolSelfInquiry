c223563c47e3ce910231ac8c89b750586c711e54
Parker Lillywhite <parkerlillywhite@gmail.com>
2024-01-17 16:15:12 -0700

    message

--- src/main/java/com/date/DateService.java
+++ src/main/java/com/date/DateService.java
@@ -31,7 +31,6 @@ public class DateService {
 
         //this::disableDate applies the disableDate to each request in the list.
         //flat map flattens the steam of lists into a single stream.
-        //change
         List<TimeCancelledResponse> responses =  requests.stream()
                 .map(this::disableDate)
                 .flatMap(List::stream)
